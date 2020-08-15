4f812e07ec65e07bc06521b71ccd8b984b4b6d4f
jbellmont <bellmont@gmail.com>
2020-08-15 11:35:31 +0100

    Basic functionality is complete.

---
+++ script.js
@@ -0,0 +1,27 @@
+const key = '923f5260b0e11473881aa435fbcf82dd';
+const searchBar = document.getElementById('search-bar');
+const submitButton = document.getElementById('submit-btn');
+let tilesUl = document.querySelector('.tile-cities');
+
+function searchCityAddTile() {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value}&appid=${key}&units=metric`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+      // Create a tile li element
+      let cityTileLi = document.createElement("LI");
+      cityTileLi.classList.add('tile-item');
+      tilesUl.appendChild(cityTileLi); // append to ul
+      cityTileLi.innerHTML = `
+        <h1 class="city-name">${data.name}</h1>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+        <h2 class="city-temp">${Math.round(data.main.temp)}°C</h2>
+        `;
+    })
+    .catch(() => console.log("error!"))
+}
+
+submitButton.addEventListener('click', searchCityAddTile);
+
