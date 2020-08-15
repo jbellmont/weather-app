647cf6b50ea98ecb55b0b017c02c78ab13aa78c9
jbellmont <bellmont@gmail.com>
2020-08-15 14:11:28 +0100

    Updated styling on button, with hover transitions. After search, the header section sshrinks and reveals the temp tiles.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const key = '923f5260b0e11473881aa435fbcf82dd';
 const searchBar = document.getElementById('search-bar');
 const submitButton = document.getElementById('submit-btn');
+const headerContainer = document.getElementById('header-contents');
+const tileContainer = document.querySelector('.tile-container');
 let tilesUl = document.querySelector('.tile-cities');
 
 function searchCityAddTile() {
@@ -10,13 +12,19 @@ function searchCityAddTile() {
     .then(data => {
       console.log(data);
 
+      // Shrink the Header
+      headerContainer.style.height = '50vh';
+      tileContainer.style.display = 'block';
+
       // Create a tile li element
       let cityTileLi = document.createElement("LI");
       cityTileLi.classList.add('tile-item');
       tilesUl.appendChild(cityTileLi); // append to ul
       cityTileLi.innerHTML = `
         <h1 class="city-name">${data.name}</h1>
+
         <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+
         <h2 class="city-temp">${Math.round(data.main.temp)}°C</h2>
         `;
     })
@@ -24,4 +32,3 @@ function searchCityAddTile() {
 }
 
 submitButton.addEventListener('click', searchCityAddTile);
-
